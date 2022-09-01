60cd0201084088e9582af3de2d7264b688350c5a
Owen Chen <owenchen@jubo.health>
2022-09-01 10:12:32 +0800

    update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
+            nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js')
         }
     });
